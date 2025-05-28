8e73cb131ea1319f8cf1881e5305cefeaa7eb7f9
Eugene <134902746+H-Eugene@users.noreply.github.com>
2025-05-28 07:37:56 +0300

    Rewrite ScreenController to class

--- script.js
+++ script.js
@@ -9,9 +9,6 @@ class GameBoard {
     }
   }
 
-  get board() { return this._board;}
-  set board(newValue) {this._board = newValue};
-
   setToken(x, y, player) {
     if (isNaN(x) || isNaN(y)) return;
     let cell = this.board[x][y];
@@ -50,8 +47,8 @@ class Player {
     this.value = value;
   }
   
-  increaseScore() {++this._score;}
-  resetScore() {this._score = 0;}
+  increaseScore() {++this.score;}
+  resetScore() {this.score = 0;}
 }
 
 class GameParameters {
@@ -64,8 +61,8 @@ class GameParameters {
 
 class GameController {
   constructor(gameParams) {
-    this.playerX = new Player("TokenX", gameParams.playerXName);
-    this.playerO = new Player("TokenO", gameParams.playerOName);
+    this.playerX = new Player("TokenX", gameParams.playerX);
+    this.playerO = new Player("TokenO", gameParams.playerO);
     this.activePlayer = this.playerX;
     this.board = new GameBoard(gameParams.boardSize);
     this.cellsLeft = this.board.board.length ** 2;
@@ -250,39 +247,43 @@ class GameController {
 }
 
 
-function ScreenController() {
-  let game = null;
-  
-  let playerTurnDiv = document.querySelector(".turn");
-  let boardDiv = document.querySelector(".board");
+class ScreenController {  
+  playerTurnDiv = document.querySelector(".turn");
+  boardDiv = document.querySelector(".board");
 
-  let restartBtn = document.querySelector(".restart");
-  let quitBtn = document.querySelector(".quit");
+  restartBtn = document.querySelector(".restart");
+  quitBtn = document.querySelector(".quit");
 
-  let playerXDiv = document.querySelector(".playerX");
-  let playerODiv = document.querySelector(".playerO");
-  let ties = document.querySelector(".ties");
+  playerXDiv = document.querySelector(".playerX");
+  playerODiv = document.querySelector(".playerO");
+  ties = document.querySelector(".ties");
 
 
-  const circleSVG = '<svg viewBox="-1.2 -1.2 10.40 10.40" id="meteor-icon-kit__regular-circle-xxs" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path fill-rule="evenodd" clip-rule="evenodd" d="M4 8C1.79086 8 0 6.2091 0 4C0 1.79086 1.79086 0 4 0C6.2091 0 8 1.79086 8 4C8 6.2091 6.2091 8 4 8zM4 6C5.1046 6 6 5.1046 6 4C6 2.8954 5.1046 2 4 2C2.8954 2 2 2.8954 2 4C2 5.1046 2.8954 6 4 6z" fill="currentColor"></path></g></svg>';
-  const crossSVG = '<svg viewBox="-3.75 -3.75 32.50 32.50" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:sketch="http://www.bohemiancoding.com/sketch/ns" fill="currentColor"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <title>cross</title> <desc>Created with Sketch Beta.</desc> <defs> </defs> <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" sketch:type="MSPage"> <g id="Icon-Set-Filled" sketch:type="MSLayerGroup" transform="translate(-469.000000, -1041.000000)" fill="currentColor"> <path d="M487.148,1053.48 L492.813,1047.82 C494.376,1046.26 494.376,1043.72 492.813,1042.16 C491.248,1040.59 488.712,1040.59 487.148,1042.16 L481.484,1047.82 L475.82,1042.16 C474.257,1040.59 471.721,1040.59 470.156,1042.16 C468.593,1043.72 468.593,1046.26 470.156,1047.82 L475.82,1053.48 L470.156,1059.15 C468.593,1060.71 468.593,1063.25 470.156,1064.81 C471.721,1066.38 474.257,1066.38 475.82,1064.81 L481.484,1059.15 L487.148,1064.81 C488.712,1066.38 491.248,1066.38 492.813,1064.81 C494.376,1063.25 494.376,1060.71 492.813,1059.15 L487.148,1053.48" id="cross" sketch:type="MSShapeGroup"> </path> </g> </g> </g></svg>';
+  circleSVG = '<svg viewBox="-1.2 -1.2 10.40 10.40" id="meteor-icon-kit__regular-circle-xxs" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path fill-rule="evenodd" clip-rule="evenodd" d="M4 8C1.79086 8 0 6.2091 0 4C0 1.79086 1.79086 0 4 0C6.2091 0 8 1.79086 8 4C8 6.2091 6.2091 8 4 8zM4 6C5.1046 6 6 5.1046 6 4C6 2.8954 5.1046 2 4 2C2.8954 2 2 2.8954 2 4C2 5.1046 2.8954 6 4 6z" fill="currentColor"></path></g></svg>';
+  crossSVG = '<svg viewBox="-3.75 -3.75 32.50 32.50" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:sketch="http://www.bohemiancoding.com/sketch/ns" fill="currentColor"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <title>cross</title> <desc>Created with Sketch Beta.</desc> <defs> </defs> <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" sketch:type="MSPage"> <g id="Icon-Set-Filled" sketch:type="MSLayerGroup" transform="translate(-469.000000, -1041.000000)" fill="currentColor"> <path d="M487.148,1053.48 L492.813,1047.82 C494.376,1046.26 494.376,1043.72 492.813,1042.16 C491.248,1040.59 488.712,1040.59 487.148,1042.16 L481.484,1047.82 L475.82,1042.16 C474.257,1040.59 471.721,1040.59 470.156,1042.16 C468.593,1043.72 468.593,1046.26 470.156,1047.82 L475.82,1053.48 L470.156,1059.15 C468.593,1060.71 468.593,1063.25 470.156,1064.81 C471.721,1066.38 474.257,1066.38 475.82,1064.81 L481.484,1059.15 L487.148,1064.81 C488.712,1066.38 491.248,1066.38 492.813,1064.81 C494.376,1063.25 494.376,1060.71 492.813,1059.15 L487.148,1053.48" id="cross" sketch:type="MSShapeGroup"> </path> </g> </g> </g></svg>';
 
-  const updateScreen = () => {
-    boardDiv.textContent = "";
 
-    const board = game.board;
-    const activePlayer = game.activePlayer;
-    const roundResult = game.roundResult;
-    const playerX = game.playerX;
-    const playerO = game.playerO;
-    const roundNumber = game.roundNumber;
+  constructor(gameParams) {
+    this.game = new GameController(gameParams);  
+    this.boardDiv.style["grid-template-columns"] = `repeat(${gameParams.boardSize}, 1fr)`;
+  }
 
-    playerTurnDiv.textContent = `${activePlayer.name}'s turn`;
-    playerXDiv.children.item(0).innerText = `${playerX.name}`;
-    playerXDiv.children.item(1).innerText = `${playerX.score}`;
-    playerODiv.children.item(0).innerText = `${playerO.name}`;
-    playerODiv.children.item(1).innerText = `${playerO.score}`;
-    ties.children.item(1).innerText = `${roundNumber - playerX.score - playerO.score}`;
+  updateScreen() {
+    this.boardDiv.textContent = "";
+
+    const board = this.game.board;
+    const activePlayer = this.game.activePlayer;
+    const roundResult = this.game.roundResult;
+    const playerX = this.game.playerX;
+    const playerO = this.game.playerO;
+    const roundNumber = this.game.roundCounter;
+
+    this.playerTurnDiv.textContent = `${activePlayer.name}'s turn`;
+    this.playerXDiv.children.item(0).innerText = `${playerX.name}`;
+    this.playerXDiv.children.item(1).innerText = `${playerX.score}`;
+    this.playerODiv.children.item(0).innerText = `${playerO.name}`;
+    this.playerODiv.children.item(1).innerText = `${playerO.score}`;
+    this.ties.children.item(1).innerText = `${roundNumber - playerX.score - playerO.score}`;
     
 
     board.board.forEach((row, x) => {
@@ -292,11 +293,11 @@ function ScreenController() {
 
 
         if (cell.value === playerX.value) {
-          cellButton.innerHTML = crossSVG;
+          cellButton.innerHTML = this.crossSVG;
         } 
 
         if (cell.value === playerO.value) {
-          cellButton.innerHTML = circleSVG;
+          cellButton.innerHTML = this.circleSVG;
         } 
 
 
@@ -306,62 +307,57 @@ function ScreenController() {
 
         cellButton.dataset.x = x;
         cellButton.dataset.y = y;
-        boardDiv.appendChild(cellButton);
+        this.boardDiv.appendChild(cellButton);
       })
     });
 
 
     if (roundResult === "win") {
-      playerTurnDiv.textContent = `${game.activePlayer.name} won!`;
-      boardDiv.removeEventListener("click", clickHandlerBoard);
+      this.playerTurnDiv.textContent = `${this.game.activePlayer.name} won!`;
+      this.boardDiv.removeEventListener("click", this.clickHandlerBoard);
     } 
     
     if (roundResult === "draw") {
-      playerTurnDiv.textContent = "It's a draw!";
-      boardDiv.removeEventListener("click", clickHandlerBoard);
+      this.playerTurnDiv.textContent = "It's a draw!";
+      this.boardDiv.removeEventListener("click", this.clickHandlerBoard);
     }
     
     }
 
-  function clickHandlerBoard(e) {
+  clickHandlerBoard = (e) => {
     const x = +e.target.dataset.x;
     const y = + e.target.dataset.y;
     
-    game.playRound(x, y);
-    updateScreen();
+    this.game.playRound(x, y);
+    this.updateScreen();
   }
 
-  function backToIndex(e) {
-    boardDiv.removeEventListener("click", clickHandlerBoard);
-    restartBtn.removeEventListener("click", game.restart);
-    quitBtn.removeEventListener("click", backToIndex);
+  backToIndex = (e) => {
+    this.boardDiv.removeEventListener("click", this.clickHandlerBoard);
+    this.restartBtn.removeEventListener("click", this.game.restart);
+    this.quitBtn.removeEventListener("click", this.backToIndex);
     window.location.replace("index.html"); 
   }
 
 
-  const run = () => {
-    const params = JSON.parse(localStorage.getItem("gameParams"));
-    let gameParams = null;
-    if (params) {
-      gameParams = new GameParameters(params.playerX, params.playerO, params.size);
-    }
-
-    game = new GameController(gameParams);  
-    boardDiv.style["grid-template-columns"] = `repeat(${gameParams.boardSize}, 1fr)`;  
+  run() {  
     
-    boardDiv.addEventListener("click", clickHandlerBoard);
-    restartBtn.addEventListener("click", 
+    this.boardDiv.addEventListener("click", this.clickHandlerBoard);
+    this.restartBtn.addEventListener("click", 
       (e) => {
-        game.restart();
-        boardDiv.addEventListener("click", clickHandlerBoard);
-        updateScreen();
+        this.game.restart();
+        this.boardDiv.addEventListener("click", this.clickHandlerBoard);
+        this.updateScreen();
       });
-    quitBtn.addEventListener("click", backToIndex);
+    this.quitBtn.addEventListener("click", this.backToIndex);
 
-    updateScreen();
+    this.updateScreen();
   }
 
-  run();
 }
 
-ScreenController();
+const params = JSON.parse(localStorage.getItem("gameParams"));
+if (params) {
+  const gameParams = new GameParameters(params.playerX, params.playerO, params.size);
+  new ScreenController(gameParams).run();
+}
